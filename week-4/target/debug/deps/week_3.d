f9762a309d002fa2b71a6e19e80c0462050e0003
i.eyoCOS101 <shinigakamii@gmail.com>
2023-11-05 15:11:39 +0100

    new commit

---
+++ week-4/target/debug/deps/week_3.d
@@ -0,0 +1,5 @@
+C:\Users\User\Documents\i.eyoCOS101\week-4\target\debug\deps\week_3.exe: src\main.rs
+
+C:\Users\User\Documents\i.eyoCOS101\week-4\target\debug\deps\week_3.d: src\main.rs
+
+src\main.rs:
---
+++ week-4/target/debug/week-3.d
@@ -0,0 +1,1 @@
+C:\Users\User\Documents\i.eyoCOS101\week-4\target\debug\week-3.exe: C:\Users\User\Documents\i.eyoCOS101\week-4\src\main.rs
